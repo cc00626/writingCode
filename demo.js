325066642d6dbb5d4b78b8e3b5ece01f93e1ef8a
cc <cc00626@outlook.com>
2026-02-24 13:07:49 +0800

    2-5

--- demo.js
+++ demo.js
@@ -1,47 +1,27 @@
-class EventBus {
-  constructor() {
-    this.events = {};
-  }
-
-  on(event, callback) {
-    if (!this.events[event]) {
-      this.events[event] = [];
-    }
-
-    this.events[event].push(callback);
-
-    return this;
-  }
-
-  emit(event, ...payload) {
-    const callbacks = this.events[event];
-
-    callbacks.forEach((fn) => {
-      fn.apply(this, ...payload);
+Promise.allSettled = (promises) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let finishedCount = 0;
+    promises.map((promise, index) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          results[index] = {
+            status: "fulfilled",
+            value,
+          };
+        })
+        .catch((reason) => {
+          results[index] = {
+            status: "rejected",
+            reason,
+          };
+        })
+        .finally(() => {
+          finishedCount++;
+          if (finishedCount === results) {
+            resolve(results);
+          }
+        });
     });
-
-    return this;
-  }
-
-  off(event, callback) {
-    if (!this.events[event]) {
-      return this;
-    }
-
-    const callbacks = this.events[event];
-    const index = callbacks.indexOf(callback);
-    callbacks.splice(index, 1);
-
-    return this;
-  }
-
-  once(evt, callback) {
-    const proxyCallback = (...payload) => {
-      callback.apply(ctx, payload);
-      // 回调函数执行完成之后就删除事件订阅
-      this.off(evt, proxyCallback);
-    };
-
-    this.on(evt, proxyCallback);
-  }
-}
+  });
+};
