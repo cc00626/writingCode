947f4116349306164f676fc71a4ec8bd9224f5e6
cc <cc00626@outlook.com>
2026-03-08 13:50:19 +0800

    2-8

--- demo.js
+++ demo.js
@@ -1,16 +1,36 @@
-let arr = [
-  { id: 1, name: "部门1", pid: 0 },
-  { id: 2, name: "部门2", pid: 1 },
-  { id: 3, name: "部门3", pid: 1 },
-  { id: 4, name: "部门4", pid: 3 },
-  { id: 5, name: "部门5", pid: 4 },
-  { id: 6, name: "部门6", pid: 0 },
-];
-
-Array.prototype.map = (callback) => {
-  let res = [];
-  for (let i = 0; i < this.length; i++) {
-    res.push(callback(this[i], i, this));
+class LazyMan {
+  constructor(name) {
+    this.name = name;
+    this.queue = [];
+    this.sayName();
+    this.run();
   }
-  return res;
-};
+
+  sayName() {
+    this.queue.push(() => {
+      console.log(this.name);
+    });
+    return this;
+  }
+
+  sleep() {
+    this.queue.push(() => this.timeWait(time));
+    return this;
+  }
+
+  timeWait(time) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    });
+  }
+
+  run() {
+    setTimeout(async () => {
+      for (const fn of this.queue) {
+        await fn();
+      }
+    }, 0);
+  }
+}
