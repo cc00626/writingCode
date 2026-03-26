7aae9daf0037fcbb9cc898ef439b704838993f99
cc <cc00626@outlook.com>
2026-03-26 21:26:26 +0800

    21

---
+++ demo.js
@@ -0,0 +1,12 @@
+const arrayData = [
+  { id: 1, name: "部门A", pid: 0 },
+  { id: 2, name: "部门B", pid: 1 },
+  { id: 3, name: "部门C", pid: 1 },
+  { id: 4, name: "部门D", pid: 2 },
+  { id: 5, name: "部门E", pid: 2 },
+  { id: 6, name: "部门F", pid: 3 },
+];
+
+const toTree = () => {
+    
+}
